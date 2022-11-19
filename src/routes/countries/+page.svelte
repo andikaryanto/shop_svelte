244954bbbb5155c8937f69488ad3489b663f44e5
Andik Aryanto <andik.aryantoo@gmail.com>
2022-11-20 02:11:20 +0700

    reactive

--- src/routes/countries/+page.svelte
+++ src/routes/countries/+page.svelte
@@ -1,26 +1,19 @@
 <script lang="ts">
-	import { getContextClient, queryStore } from "@urql/svelte";
-	import TableCell from "../../components/tables/TableCell.svelte";
-	import TableItem from "../../components/tables/TableItem.svelte";
-	import Table from "../../components/tables/Table.svelte";
 	import Container from "../../page_components/Container.svelte";
 	import Countries from "../../resources/Countries";
 	import CustomableList from "../../page_components/list/CustomableList.svelte";
 
   import {listPageStore} from  '../../stores/listPageStore';
 
-  // let countries: any = {};
-  // listPageStore.subscribe(listStore => {
-    let countries = Countries
-    .setIdentityParameters('($page: Int, $size: Int $search_value: String)')
-    .setQueryParameters('(page: $page size: $size search_value: $search_value)')
-    .setVariables({
-      page: $listPageStore.page,
-      size: $listPageStore.size,
-      search_value: $listPageStore.search_value
-    })
-    .fetch();
-  // })
+    $: countries = Countries
+      .setIdentityParameters('($page: Int, $size: Int $search_value: String)')
+      .setQueryParameters('(page: $page size: $size search_value: $search_value)')
+      .setVariables({
+        page: $listPageStore.page,
+        size: $listPageStore.size,
+        search_value: $listPageStore.search_value
+      })
+      .fetch();
   
 </script>
 
